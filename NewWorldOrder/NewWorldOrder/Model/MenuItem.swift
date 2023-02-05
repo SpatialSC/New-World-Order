d8cb4d71d5600cbdac5783761a58d885528eacbc
Jacqueline Guo <jacquelineguo7@gmail.com>
2023-02-05 01:57:49 -0800

    Updated Menu Items Struct (Ingredients and Images)

--- NewWorldOrder/NewWorldOrder/Model/MenuItem.swift
+++ NewWorldOrder/NewWorldOrder/Model/MenuItem.swift
@@ -11,50 +11,45 @@ struct MenuItem: Equatable, Codable {
     let title: String
     let price: Double
     let image: String
+    let ingredients: String
 }
 
 let AllMenuItems: [(String, [MenuItem])] = [
     ("Drinks",
-     [MenuItem(title: "Donut Blend", price: 3.50, image: "matcha"),
-      MenuItem(title: "Joe Pro", price: 3.75, image: "matcha"),
-      MenuItem(title: "Espresso", price: 3.25, image: "matcha"),
-      MenuItem(title: "Macchiato", price: 3.50, image: "matcha"),
-      MenuItem(title: "Cortado", price: 3.75, image: "matcha"),
-      MenuItem(title: "Cappuccino", price: 4.00, image: "matcha"),
-      MenuItem(title: "Americano", price: 3.25, image: "matcha"),
-      MenuItem(title: "Latte", price: 4.75, image: "matcha"),
-      MenuItem(title: "Dulce Latte", price: 5.00, image: "matcha"),
-      MenuItem(title: "Mocha", price: 5.25, image: "matcha"),
-      MenuItem(title: "Cold Brew", price: 4.00, image: "matcha"),
-      MenuItem(title: "Hot Chocolate", price: 4.75, image: "matcha"),
-      MenuItem(title: "Matcha Latte", price: 4.50, image: "matcha"),
+     [MenuItem(title: "Americano", price: 3.25, image: "matcha", ingredients: "Espresso Coffee, Water"),
+      MenuItem(title: "Ice Latte", price: 4.75, image: "icelatte", ingredients: "Espresso Coffee, Milk of Choice, Vanilla Syrup, Ice"),
+      MenuItem(title: "Mocha", price: 5.25, image: "matcha", ingredients: "Espresso Roast, Milk of Choice, Mocha Sauce, Whipped Cream"),
+      MenuItem(title: "Cold Brew", price: 4.00, image: "coldbrew", ingredients: "Steeped Coffee, Sweet Cream, Ice"),
+      MenuItem(title: "Hot Chocolate", price: 4.75, image: "hotchocolate", ingredients: "Milk of Choice, Cocoa powder, Chocolate, Vanilla Extract, Sugar"),
+      MenuItem(title: "Matcha Latte", price: 4.50, image: "matcha", ingredients: "Matcha Powder, Hot Water, Milk of Choice, Honey, Ice"),
      ]
     ),
     ("Breakfast",
-     [MenuItem(title: "Breakfast Burrito", price: 8.75, image: "matcha"),
-      MenuItem(title: "Breakfast Sandwich", price: 8.75, image: "matcha"),
-      MenuItem(title: "Ranchera Steak Burrito", price: 10.00, image: "matcha"),
+     [MenuItem(title: "Breakfast Burrito", price: 8.75, image: "matcha", ingredients: "Tortilla, Avocados, Tomatoes, Mushrooms, Shallots, Garlic, Lime Juice, Eggs, Sausage, Monterey Jack Cheese, Seasonings (Cumin, Cilantro, Salt, Pepper)"),
+      MenuItem(title: "Breakfast Sandwich", price: 8.75, image: "matcha", ingredients: "Croissant, Bacon, Eggs, Mushrooms, American Cheese, Avocados, Seasonings (Salt, Pepper)"),
+      MenuItem(title: "Ranchera Steak Burrito", price: 10.00, image: "matcha", ingredients: "Tortilla, Steak, Pico de Gallo (Tomatoes, Onions, Peppers, Lime Juice, Cilantro, Black Beans, Cheddar Cheese, Rice"),
      ]
     ),
     ("Sandwiches",
-     [MenuItem(title: "Pesto Chicken", price: 9.75, image: "matcha"),
-      MenuItem(title: "Dulce Club", price: 9.75, image: "matcha"),
-      MenuItem(title: "Apple Tuna", price: 9.75, image: "matcha"),
-      MenuItem(title: "Turkey Swiss", price: 8.50, image: "matcha"),
-      MenuItem(title: "Spicy Korean Cheesesteak", price: 10.25, image: "matcha"),
+     [MenuItem(title: "Pesto Chicken", price: 9.75, image: "matcha", ingredients: "Bread Rolls, Chicken, Mozzarella Slices, Tomatoes, Pesto"),
+      MenuItem(title: "Dulce Club", price: 9.75, image: "matcha", ingredients: "Baguette Bread, Turkey, Bacon, Tomatoes, Lettuce, Avocado, Mustard, Mayo"),
+      MenuItem(title: "Apple Tuna", price: 9.75, image: "matcha", ingredients: "Sliced Bread, Tuna, Celery, Onion, Relish, Chopped Apple, Lettuce, Mayo"),
+      MenuItem(title: "Turkey Swiss", price: 8.50, image: "matcha", ingredients: "French Bread, Roast Turkey, Swiss Cheese, Tomatoes, Onion, Mustard, Mayo"),
+      MenuItem(title: "Spicy Korean Cheesesteak", price: 10.25, image: "matcha", ingredients: "Hoagie Buns, Bulgogi sliced beef, Kimchi, Onions, Scallions, Garlic, Provolone Cheese, Gochujang Mayo"),
+      MenuItem(title: "Falafel Pita", price: 10.25, image: "matcha", ingredients: "Pita Bread, Falafel, Hummus, Eggplant, Pickles, Cabbage, Carrots, Tomato, Onion, Garlic Dressing, Skhug"),
      ]
     ),
     ("Salads",
-     [MenuItem(title: "Peanut Kale", price: 9.50, image: "matcha"),
-      MenuItem(title: "Caprese", price: 9.50, image: "matcha"),
-      MenuItem(title: "Asian Chicken", price: 10.00, image: "matcha"),
-      MenuItem(title: "Cranberry Walnut", price: 9.00, image: "matcha"),
+     [MenuItem(title: "Peanut Kale", price: 9.50, image: "matcha", ingredients: "Kale, Onions, Garlic, Peanut Butter, Rice Vinegar, Tamari, Rice Syrup, Honey"),
+      MenuItem(title: "Caprese", price: 9.50, image: "matcha", ingredients: "Basil, Tomatoes, Mozerella, Balsamic Vinegar, Olive Oil"),
+      MenuItem(title: "Asian Chicken", price: 10.00, image: "matcha", ingredients: "Cabbage, Lettuce, Shredded Chicken, Carrots, Red Peppers, Shallots, Rice Vinegar, Soy Sauce, Garlic, Sesame Oil, Sesame Seeds, Mandarin Oranges"),
+      MenuItem(title: "Cranberry Walnut", price: 9.00, image: "matcha", ingredients: "Lettuce, Spinach, Walnuts, Red Onion, Cranberries, Goat Cheese, Balsamic Vinaigrette"),
      ]
     ),
     ("Dinner",
-     [MenuItem(title: "Bacon Avocado Burger", price: 11.50, image: "matcha"),
-      MenuItem(title: "Dulce Burger", price: 9.75, image: "matcha"),
-      MenuItem(title: "Tuna Melt", price: 9.50, image: "matcha"),
+     [MenuItem(title: "Bacon Avocado Burger", price: 11.50, image: "matcha", ingredients: "Toasted Burger Bun, Bacon, Ground Beef Burger Meat, Lettuce, Tomatoes, Cheddar Cheese, Onions, Avocado, Mayo"),
+      MenuItem(title: "Vegan Burger", price: 9.75, image: "matcha", ingredients: "Toasted Burger Buns, Spinach, Roasted Portobello Mushrooms, Onions, Garlic, Shallots, Avocado"),
+      MenuItem(title: "Tuna Melt", price: 9.50, image: "matcha", ingredients: "Sourdough Bread, Tuna, Spinach, Celery, Onion, Tomato, Pickles, Red Peppers, Tomatoes, Cheddar Cheese, Lemon Juice, Mayo"),
      ]
     ),
 ]
